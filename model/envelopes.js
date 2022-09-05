c5d890a61f8fa9f4c6bfa6d898ec423a73773ca8
Michael Schlueter <schluet0r4life@gmail.com>
2022-09-05 16:23:25 +0200

    update folder structure, create model for envelopes

---
+++ model/envelopes.js
@@ -0,0 +1,24 @@
+"use strict";
+const envelopes = [
+    {
+        id: 1,
+        title: 'groceries',
+        budget: 100,
+    },
+    {
+        id: 2,
+        title: 'rent',
+        budget: 400,
+    },
+    {
+        id: 3,
+        title: 'clothing',
+        budget: 50,
+    },
+    {
+        id: 4,
+        title: 'entertainment',
+        budget: 100,
+    }
+];
+module.exports = envelopes;
