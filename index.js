78f3450eef571bdc5e800bf70ca79d67df97b35e
Michael Schlueter <schluet0r4life@gmail.com>
2022-09-05 16:14:11 +0200

    implement typescript

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
-const express = require("express");
-const dotenv = require("dotenv");
-
-dotenv.config();
-
-const app = express();
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config();
+const app = (0, express_1.default)();
 const port = process.env.PORT;
-
 app.get("/", (req, res) => {
-  res.send("Hello World");
+    res.send("Hello World, Hello Michael");
 });
-
 app.listen(port, () => {
-  console.log(`[server]: Server is running at http://localhost:${port}`);
+    console.log(`[server]: Server is running at http://localhost:${port}`);
 });
